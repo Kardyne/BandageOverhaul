1d6d7c2543445e97a39848f959ef3e8ba7fe7a12
Kardyne <Kardyne@users.noreply.github.com>
2022-02-10 10:49:32 +0100

    refactor: split Patches.cs into two files
    One contains (ugly) workarounds to avoid errors ar runtime for some TLD methods.
    The other contains actual features for the mod.
    The goal is to make it easier to maintain by having smaller files.

--- Code/patches/Workarounds.cs
+++ Code/patches/Workarounds.cs
@@ -1,61 +1,10 @@
-﻿using HarmonyLib;
-using UnityEngine;
+﻿/// This file contains workarounds to avoid errors at runtime in TLD methods.
+using HarmonyLib;
 
 namespace BandageOverhaul
 {
-    internal class Patches
+    internal class Workarounds
     {
-        [HarmonyPatch(typeof(GameManager), "Awake")]
-        internal class GameManager_Awake
-        {
-            private static void Postfix()
-            {
-                RemoveBandageBlueprints();
-                ChangeClothHarvest();
-            }
-
-            private static void RemoveBandageBlueprints()
-            {
-                foreach (BlueprintItem bpi in Utils.FindBlueprintsForGear("GEAR_HeavyBandage"))
-                {
-                    bpi.enabled = false;
-                }
-            }
-
-            private static void ChangeClothHarvest()
-            {
-                Harvest clothHarvestPrefab = Resources.Load<GameObject>("GEAR_Cloth")?.GetComponent<Harvest>();
-                GearItem dirtyBandageGearItem = Resources.Load("assets/prefabs/gear_dirtybandage.prefab")?.TryCast<GameObject>()?.GetComponent<GearItem>();
-                if (clothHarvestPrefab == null || dirtyBandageGearItem == null)
-                {
-                    return;
-                }
-                clothHarvestPrefab.m_YieldGear = new GearItem[] { dirtyBandageGearItem };
-                clothHarvestPrefab.m_YieldGearUnits = new int[] { 2 };
-            }
-        }
-
-        [HarmonyPatch(typeof(BloodLoss), "BloodLossEnd")]
-        internal class BloodLoss_BloodLossEnd
-        {
-            private static void Postfix(BloodLoss __instance, int index)
-            {
-                FirstAidItem firstAidItem = GameManager.GetPlayerManagerComponent()?.m_FirstAidItemUsed;
-                if (firstAidItem == null)
-                {
-                    return;
-                }
-                InfectionRisk infectionRisk = GameManager.GetInfectionRiskComponent();
-                AfflictionBodyArea afflictionBodyArea = __instance.GetLocation(index);
-                int infectionRiskIndex = infectionRisk.GetIndexFromLocation(afflictionBodyArea);
-                GearItem gi = firstAidItem.GetComponent<GearItem>();
-                if (infectionRiskIndex >= 0 && gi?.m_GearName == "GEAR_DirtyBandage")
-                {
-                    infectionRisk.m_CurrentInfectionChanceList[index] += Settings.instance.infectionRiskIncrease;
-                }
-            }
-        }
-
         [HarmonyPatch(typeof(CookingPotItem), "SetCookedGearProperties")]
         internal class CookingPotItem_SetCookedGearProperties
         {
